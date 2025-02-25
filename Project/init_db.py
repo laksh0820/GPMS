6e80c9eeec1a8cd2a1f8d3d01eee79568be353e4
Lakshya <jklagrawal123@gmail.com>
2025-02-25 16:53:29 +0530

    Updates

--- Project/init_db.py
+++ Project/init_db.py
@@ -22,7 +22,7 @@ db = conn.db()
 
 # Create Users Table
 db.execute("""
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS user (
         id SERIAL PRIMARY KEY,
         email VARCHAR(100) UNIQUE NOT NULL,
         password VARCHAR(200) NOT NULL,
@@ -33,9 +33,8 @@ db.execute("""
     """)
 
 # Create Household Table
-db.execute(
-    """
-    CREATE TABLE Household (
+db.execute("""
+    CREATE TABLE IF NOT EXISTS Household (
         household_id INT,
         address VARCHAR(50),
         income DECIMAL(12,4),
@@ -45,7 +44,7 @@ db.execute(
 
 # Create Citizen Table
 db.execute("""
-    CREATE TABLE Citizen (
+    CREATE TABLE IF NOT EXISTS Citizen (
         citizen_id INT,
         name VARCHAR(50),
         gender VARCHAR(10),
@@ -59,7 +58,7 @@ db.execute("""
 
 # Create Panchayat Employee Table
 db.execute("""
-    CREATE TABLE Panchayat_Employee (
+    CREATE TABLE IF NOT EXISTS Panchayat_Employee (
         employee_id INT,
         citizen_id INT,
         role VARCHAR(50),
@@ -70,7 +69,7 @@ db.execute("""
 
 # Create Asset Table
 db.execute("""
-    CREATE TABLE Asset (
+    CREATE TABLE IF NOT EXISTS Asset (
         asset_id INT,
         employee_id INT,
         type VARCHAR(50),
@@ -83,7 +82,7 @@ db.execute("""
 
 # Create Agriculture Table
 db.execute("""
-    CREATE TABLE Land_Record (
+    CREATE TABLE IF NOT EXISTS Land_Record (
         land_id INT,
         citizen_id INT,
         area_acres DECIMAL(10,4),
@@ -95,7 +94,7 @@ db.execute("""
 
 # Create Vaccination Table
 db.execute("""
-    CREATE TABLE Vaccination (
+    CREATE TABLE IF NOT EXISTS Vaccination (
         vaccination_id INT,
         citizen_id INT,
         vaccine_type VARCHAR(50),
@@ -107,7 +106,7 @@ db.execute("""
 
 # Create Welfare-Scheme Table
 db.execute("""
-    CREATE TABLE Welfare_Scheme (
+    CREATE TABLE IF NOT EXISTS Welfare_Scheme (
         scheme_id INT,
         name VARCHAR(50),
         description VARCHAR(1000),
@@ -117,7 +116,7 @@ db.execute("""
 
 # Create Scheme-Enrollment Table
 db.execute("""
-    CREATE TABLE Scheme_Enrollment (
+    CREATE TABLE IF NOT EXISTS Scheme_Enrollment (
         enrollment_id INT,
         citizen_id INT,
         scheme_id INT,
@@ -129,9 +128,8 @@ db.execute("""
     """)
 
 # Create a Census Data Table
-db.execute(
-    """
-    CREATE TABLE Census_Data (
+db.execute("""
+    CREATE TABLE IF NOT EXISTS Census_Data (
         data_id INT,
         household_id INT,
         citizen_id INT,
