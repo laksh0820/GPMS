6839ff36dc45b553447cb1cd606454b125ba1411
@kmohanb2003 <kmohanb2003@kgpian.iitkgp.ac.in>
2025-02-26 11:49:06 +0530

    Minor Updates and addition of Logo to the Project Folder

--- Project/Government/government.py
+++ Project/Government/government.py
@@ -4,4 +4,4 @@ government_bp = Blueprint('government',__name__,url_prefix='/government', templa
 @government_bp.route('/')
 def govenrment_base():
     return render_template('government_dashboard.html')
-    return "Hello, Government Monitor"
+
