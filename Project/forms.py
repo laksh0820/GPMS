0733118c3088bd433c4c0e4c8518f6dca46c4396
Lakshya <jklagrawal123@gmail.com>
2025-02-26 11:49:03 +0530

    Minor Update

--- Project/forms.py
+++ Project/forms.py
@@ -12,7 +12,7 @@ class UserForm(FlaskForm):
                                         Length(min=MIN_PASSWORD_LEN,max=MAX_PASSWORD_LEN),
                                         EqualTo('confirm_password',message="Password does not match to Confirm Password. Please retype"),
                                         Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]",
-                                            message="Password should contain atleast one lowercase character, one uppercase character,one number and one special character." )])
+                                        message="Password should contain atleast one lowercase character, one uppercase character,one number and one special character." )])
     confirm_password = PasswordField("Confirm-Password",validators=[DataRequired(),Length(min=MIN_PASSWORD_LEN,max=MAX_PASSWORD_LEN)])
     
 class LogInForm(FlaskForm):
