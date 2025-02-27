7f5dbe8c7b9d46dc818c0f74aca9f95cf0e033f5
Lakshya <jklagrawal123@gmail.com>
2025-02-27 23:22:41 +0530

    Updated Citizen Dashboard

--- Project/Citizen/citizen.py
+++ Project/Citizen/citizen.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,render_template,flash,redirect,url_for
+from flask import Blueprint,render_template,flash,redirect,url_for,request
 from flask_login import login_required,current_user
 from Project.utils.db_utils import get_db_connection
 
-citizen_bp = Blueprint('citizen',__name__,url_prefix='/citizen', template_folder='templates')
+citizen_bp = Blueprint('citizen',__name__,url_prefix='/citizen', template_folder='templates',static_folder='static')
 
 # Wrapper to ensure only citizen can access the page
 def citizen_required(inner_func):
@@ -38,21 +38,47 @@ def base():
 @citizen_required
 # @verification_required
 def welfare_scheme():
-    conn = get_db_connection()
-    db = conn.cursor()
+    request_type = request.args.get('request_type')
     
-    db.execute("""
-               SELECT * 
-               from welfare_scheme;
-            """)
-    res = db.fetchall()
+    scheme_title = []
+    scheme_description = []
+    enrollment_date = []
     
-    scheme_title = [row[1] for row in res]
-    scheme_description = [row[2] for row in res] 
-    
-    db.close()
-    conn.close()
-    return render_template('welfare_scheme.html',scheme_title=scheme_title,scheme_description=scheme_description)
+    if request_type == 'Active':
+        conn = get_db_connection()
+        db = conn.cursor()
+        
+        db.execute("""
+                SELECT name,description
+                from welfare_scheme;
+                """)
+        res = db.fetchall()
+        
+        scheme_title = [row[0] for row in res]
+        scheme_description = [row[1] for row in res] 
+        
+        db.close()
+        conn.close()
+    else:
+        conn = get_db_connection()
+        db = conn.cursor()
+        
+        db.execute("""
+                SELECT name,description,enrollment_date
+                FROM welfare_scheme natural join scheme_enrollment
+                WHERE citizen_id = %s;
+                """,[current_user.citizen_id])
+        res = db.fetchall()
+        
+        scheme_title = [row[0] for row in res]
+        scheme_description = [row[1] for row in res] 
+        enrollment_date = [row[2] for row in res]
+        
+        db.close()
+        conn.close()
+        
+    return render_template('welfare_scheme.html',scheme_title=scheme_title,scheme_description=scheme_description,
+                                                enrollment_date=enrollment_date,request_type=request_type)
 
 # Vaccinations 
 @citizen_bp.route('/vaccination')
@@ -60,22 +86,47 @@ def welfare_scheme():
 @citizen_required
 # @verification_required
 def vaccination():
-    conn = get_db_connection()
-    db = conn.cursor()
+    request_type = request.args.get('request_type')
     
-    db.execute("""
-                SELECT vaccine_type, date_administered
-                from vaccination
-                where citizen_id = %s;
-            """,[current_user.citizen_id])
+    vaccine_type = []
+    centers = []
+    date_administered = []
     
-    res = db.fetchall()
-    vaccination_type = [row[0] for row in res]
-    date_administered = [row[1] for row in res]
+    if request_type == 'Active':
+        conn = get_db_connection()
+        db = conn.cursor()
+        
+        db.execute("""
+                    SELECT vaccine_type, centers
+                    from vaccines;
+                """)
+        
+        res = db.fetchall()
+        vaccine_type = [row[0] for row in res]
+        center = [row[1] for row in res]
+        
+        db.close()
+        conn.close()
+    else:
+        conn = get_db_connection()
+        db = conn.cursor()
+        
+        db.execute("""
+                    SELECT vaccine_type, centers, date_administered
+                    FROM vaccines natural join vaccination
+                    WHERE citizen_id = %s;
+                """,[current_user.citizen_id])
+        
+        res = db.fetchall()
+        vaccine_type = [row[0] for row in res]
+        centers = [row[1] for row in res]
+        date_administered = [row[2] for row in res]
+        
+        db.close()
+        conn.close()
     
-    db.close()
-    conn.close()
-    return render_template('vaccination.html',vaccination_type=vaccination_type,date_administered=date_administered)
+    return render_template('vaccination.html',vaccine_type=vaccine_type,centers=centers,
+                           date_administered=date_administered,request_type=request_type)
 
 # Taxes
 @citizen_bp.route('/taxes')
@@ -109,4 +160,19 @@ def taxes():
 @citizen_required
 # @verification_required
 def service():
-    return render_template('service.html')
+    conn = get_db_connection()
+    db = conn.cursor()
+    
+    db.execute("""
+               SELECT issue_date,doc_type,description
+               FROM service natural join documents
+               WHERE citizen_id = %s;
+            """,[current_user.citizen_id])
+    res = db.fetchall()
+    issue_date = [row[0] for row in res]
+    doc_type = [row[1] for row in res]
+    description = [row[2] for row in res]
+    
+    db.close()
+    conn.close()
+    return render_template('service.html',issue_date=issue_date,doc_type=doc_type,description=description)
