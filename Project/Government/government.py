49d460d0dd3e6c3594958f5a18ccc87e429900fe
@kmohanb2003 <kmohanb2003@kgpian.iitkgp.ac.in>
2025-02-28 13:14:56 +0530

    Changed the block name of styles to extra_header and addition of Agricultural Data Table to government dashboard

--- Project/Government/government.py
+++ Project/Government/government.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_user,login_required,current_user,logout_user
 from flask_wtf import FlaskForm
 from Project.utils.db_utils import get_db_connection
@@ -13,18 +13,18 @@ def government_monitor_required(inner_func):
     wrapped_function_government_monitor.__name__ = inner_func.__name__
     return wrapped_function_government_monitor
 
-@government_bp.route('/')
+@government_bp.route('/refresh_agricultural_data')
 @login_required
 @government_monitor_required
-def base():
+def refresh_agricultural_data():
     conn = get_db_connection()
     db = conn.cursor()
 
     # Gather Information of Land covered by each crop type and Average Land covered by each crop type
     db.execute("""
-               SELECT crop_type, avg(area_acres), sum(area_acres)
-               FROM land_record
-               GROUP BY crop_type;
+                SELECT crop_type, avg(area_acres), sum(area_acres)
+                FROM land_record
+                GROUP BY crop_type;
             """)
     res = db.fetchall()
 
@@ -34,12 +34,57 @@ def base():
     total_area_acres = [row[2] for row in res]
 
 
-    # Gather Information of Average Land covered by a household in the village
-    # db.execute("""
-
-    #            """)
+    # Gather Information of Average Land covered by a citizen in the village
+    db.execute("""
+                SELECT avg(area_acres)
+                FROM land_record;
+            """)
+    res = db.fetchall()
+    avg_area_acres_per_citizen = res[0][0]
     
+
+    # Gather Information of average income of a farmer in the village
+
     db.close()
     conn.close()
 
-    return render_template('Government/dashboard.html')
+    return jsonify({'crop_type':crop_type, 'avg_area_acres':avg_area_acres, 'total_area_acres':total_area_acres, 'avg_area_acres_per_citizen':avg_area_acres_per_citizen})
+    
+
+
+@government_bp.route('/')
+@login_required
+@government_monitor_required
+def base():
+    conn = get_db_connection()
+    db = conn.cursor()
+
+    # Gather Information of Land covered by each crop type and Average Land covered by each crop type
+    db.execute("""
+                SELECT crop_type, avg(area_acres), sum(area_acres)
+                FROM land_record
+                GROUP BY crop_type;
+            """)
+    res = db.fetchall()
+
+    # Extracting relevant information
+    crop_type = [row[0] for row in res]
+    avg_area_acres = [row[1] for row in res]
+    total_area_acres = [row[2] for row in res]
+
+
+    # Gather Information of Average Land covered by a citizen in the village
+    db.execute("""
+                SELECT avg(area_acres)
+                FROM land_record;
+            """)
+    res = db.fetchall()
+    avg_area_acres_per_citizen = res[0][0]
+    
+
+    # Gather Information of average income of a farmer in the village
+
+    db.close()
+    conn.close()
+
+    return render_template('Government/dashboard.html', crop_type=crop_type, avg_area_acres=avg_area_acres, total_area_acres=total_area_acres, avg_area_acres_per_citizen=avg_area_acres_per_citizen)
