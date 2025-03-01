6b57e0546336d55a827fc3fcd7dba6416b95c0ab
Lakshya <jklagrawal123@gmail.com>
2025-03-01 15:38:44 +0530

    Updates Tax Generation Function

--- Project/__init__.py
+++ Project/__init__.py
@@ -5,6 +5,8 @@ from Project.Admin.admin import admin_bp
 from Project.Citizen.citizen import citizen_bp
 from Project.Employee.employee import employee_bp
 from Project.Government.government import government_bp
+from Project.utils.db_utils import get_db_connection
+from datetime import datetime
 
 DB_NAME = "22CS10036"
 DB_USER = "22CS10036"
@@ -27,5 +29,59 @@ app.register_blueprint(admin_bp)
 app.register_blueprint(citizen_bp)
 app.register_blueprint(employee_bp)
 app.register_blueprint(government_bp)
+    
+
+# To Calculate Tax amount
+def calculate_tax_amount(amount):
+    return 0.2 * float(amount) # Apply 20% tax on the given amounts
+
+# Helper function to calculate date after one month
+def calculate_date(skip_month=1):
+    today = datetime.today()
+    
+    if today.month == 12:
+        one_month_later = today.replace(year=today.year + 1,month=1)
+    else:
+        one_month_later = today.replace(month=today.month + 1)
+    
+    return one_month_later.strftime('%Y-%m-01')
+
+# Tax Generator Function
+def tax_generate():
+    with app.app_context():
+        conn = get_db_connection()
+        db = conn.cursor()
+        
+        current_year = datetime.today().year
+        current_month = datetime.today().month
+        db.execute("""
+                SELECT tax_year,tax_month 
+                from taxes;
+                """)
+        res = db.fetchall()
+        
+        is_available = 0
+        for row in res:
+            if row[0] == current_year and row[1] == current_month:
+                is_available = 1    
+        
+        if is_available == 0:
+            db.execute("""
+                    SELECT citizen_id,income
+                    from citizen;
+                    """)
+            res = db.fetchall()
+            
+            for row in res:
+                db.execute("""
+                        insert into taxes(citizen_id,type,tax_month,tax_year,amount_due,due_date,status)
+                        values (%s,'Income Tax',%s,%s,%s,%s,'Unpaid');
+                        """,[row[0],current_month,current_year,calculate_tax_amount(row[1]),calculate_date()])
+            conn.commit()
+        
+        db.close()
+        conn.close()
+
+tax_generate()
 
 from Project import routes
