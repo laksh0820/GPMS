9e445d864414422e7228e672d3455854e8fc3ea6
@kmohanb2003 <kmohanb2003@kgpian.iitkgp.ac.in>
2025-03-02 00:23:12 +0530

    Completed the Government Dashboard altogether
    Minor fixes in padding are yet to be done.
    Padding issues:
    1. Sidebar nav -links in Government Webpage
    2. In Census Data Tab of Government Dashboard, the rightmost column is touching the right end of the web page
    
    If you are able to fix this, you are free to do so

--- Project/Government/government.py
+++ Project/Government/government.py
@@ -4,6 +4,9 @@ from flask_wtf import FlaskForm
 from Project.utils.db_utils import get_db_connection
 government_bp = Blueprint('government',__name__,url_prefix='/government', template_folder='templates', static_folder='static')
 
+
+
+# Wrapper to ensure that the user is a gvovernment monitor or a admin
 def government_monitor_required(inner_func):
     def wrapped_function_government_monitor(*args,**kwargs):
         if current_user.is_authenticated and current_user.role != 'government' and current_user.role != 'admin':
@@ -23,11 +26,22 @@ def verification_required(inner_func):
     wrapper.__name__ = inner_func.__name__
     return wrapper
 
+
+
+
+
 @government_bp.route('/')
 @login_required
 @government_monitor_required
 @verification_required
 def base():
+    return render_template('base.html')
+
+
+
+
+
+def fetch_agricultural_data():
     conn = get_db_connection()
     db = conn.cursor()
 
@@ -36,7 +50,7 @@ def base():
                 SELECT crop_type, avg(area_acres), sum(area_acres)
                 FROM land_record
                 GROUP BY crop_type;
-                """)
+            """)
     res = db.fetchall()
 
     # Extracting relevant information
@@ -49,7 +63,7 @@ def base():
     db.execute("""
                 SELECT avg(area_acres)
                 FROM land_record;
-                """)
+            """)
     res = db.fetchall()
     avg_area_acres_per_citizen = res[0][0]
     
@@ -66,12 +80,138 @@ def base():
     db.close()
     conn.close()
 
-    return render_template('Government/dashboard.html', 
-                           crop_type=crop_type, 
-                           avg_area_acres=avg_area_acres, 
-                           total_area_acres=total_area_acres, 
-                           avg_area_acres_per_citizen=avg_area_acres_per_citizen,
-                           avg_income_per_farmer=avg_income_per_farmer)
+    return crop_type, avg_area_acres, total_area_acres, avg_area_acres_per_citizen, avg_income_per_farmer
+
+
+
+def fetch_vaccination():
+    conn = get_db_connection()
+    db = conn.cursor()
+
+    # Gather Information of the number of citizens vaccinated with each vaccine type
+    db.execute("""
+                SELECT vaccine_type, count(citizen_id)
+                FROM Vaccination JOIN Vaccines USING (vaccine_id)
+                GROUP BY vaccine_type;
+                """)
+    res = db.fetchall()
+    vaccine_type = [row[0] for row in res]
+    num_citizens = [row[1] for row in res]
+
+
+    # Gather Information of the number of citizens who have been vaccinated (taken at least one vaccine)
+    db.execute("""
+                SELECT count(distinct citizen_id)
+                FROM Vaccination;
+                """)
+    res = db.fetchall()
+    num_citizens_vaccinated = len(res)
+
+
+    # Gather Information of the number of citizens who have been vaccinated by all the available vaccine types
+    db.execute("""
+                SELECT count(citizen_id)
+                FROM Vaccination
+                GROUP BY citizen_id
+                HAVING count(vaccine_id) = (SELECT count(vaccine_id) FROM Vaccines);
+                """)
+    res = db.fetchall()
+    num_citizens_vaccinated_all = len(res)
+
+
+    # Gather Information of the number of citizens who have not been vaccinated
+    db.execute("""
+                SELECT count(citizen_id)
+                FROM Citizen
+                WHERE citizen_id NOT IN (SELECT citizen_id FROM Vaccination);
+                """)
+    res = db.fetchall()
+    num_citizens_not_vaccinated = len(res)
+
+
+    # Gather Information of the top 5 centers with the most number of vaccinations
+    db.execute("""
+                SELECT centers
+                FROM Vaccines
+                GROUP BY centers
+                ORDER BY count(vaccine_id) DESC
+                LIMIT 5;
+                """)
+    res = db.fetchall()
+    top_5_centers = [row[0] for row in res]
+
+
+    db.close()
+    conn.close()
+
+    return vaccine_type, num_citizens, num_citizens_vaccinated, num_citizens_vaccinated_all, num_citizens_not_vaccinated, top_5_centers
+
+
+
+def fetch_census_data():
+    conn = get_db_connection()
+    db = conn.cursor()
+
+    # Gather Information about the census_data
+    db.execute("""
+                SELECT year, population_male, population_female, births_male, 
+                births_female, deaths_male, deaths_female, marriages 
+                FROM census_data;
+                """)
+    res = db.fetchall()
+    years = [row[0] for row in res]
+    population_male = [row[1] for row in res]
+    population_female = [row[2] for row in res]
+    births_male = [row[3] for row in res]
+    births_female = [row[4] for row in res]
+    deaths_male = [row[5] for row in res]
+    deaths_female = [row[6] for row in res]
+    marriages = [row[7] for row in res]
+
+    db.close()
+    conn.close()
+
+    return years, population_male, population_female, births_male, births_female, deaths_male, deaths_female, marriages
+
+
+
+def fetch_environmental_data():
+    conn = get_db_connection()
+    db = conn.cursor()
+
+    # Gather Information about the last 5 days 
+    db.execute("""
+                SELECT date, air_quality_index, water_quality, sanitation
+                FROM environmental_data
+                ORDER BY date DESC
+                LIMIT 5;
+                """)
+    res = db.fetchall()
+    dates = [row[0] for row in res]
+    air_quality_index = [row[1] for row in res]
+    water_quality = [row[2] for row in res]
+    sanitation = [row[3] for row in res]
+
+
+    # Gather Information about the worst 5 days in terms of air quality index
+    db.execute("""
+                SELECT date, air_quality_index
+                FROM environmental_data
+                ORDER BY air_quality_index DESC
+                LIMIT 5;
+               """)
+    res = db.fetchall()
+    air_dates = [row[0] for row in res]
+    air_quality_index_5 = [row[1] for row in res]
+
+
+    db.close()
+    conn.close()
+
+    return dates, air_quality_index, water_quality, sanitation, air_dates, air_quality_index_5
+
+
+
 
 
 @government_bp.route('/refresh_agricultural_data')
@@ -79,43 +219,7 @@ def base():
 @government_monitor_required
 @verification_required
 def refresh_agricultural_data():
-    conn = get_db_connection()
-    db = conn.cursor()
-
-    # Gather Information of Land covered by each crop type and Average Land covered by each crop type
-    db.execute("""
-                SELECT crop_type, avg(area_acres), sum(area_acres)
-                FROM land_record
-                GROUP BY crop_type;
-            """)
-    res = db.fetchall()
-
-    # Extracting relevant information
-    crop_type = [row[0] for row in res]
-    avg_area_acres = [row[1] for row in res]
-    total_area_acres = [row[2] for row in res]
-
-
-    # Gather Information of Average Land covered by a citizen in the village
-    db.execute("""
-                SELECT avg(area_acres)
-                FROM land_record;
-            """)
-    res = db.fetchall()
-    avg_area_acres_per_citizen = res[0][0]
-    
-
-    # Gather Information of average income of a farmer in the village
-    db.execute("""
-                SELECT avg(income)
-                FROM citizen JOIN land_record USING (citizen_id);
-                """)
-    res = db.fetchall()
-    avg_income_per_farmer = res[0][0]
-
-
-    db.close()
-    conn.close()
+    crop_type, avg_area_acres, total_area_acres, avg_area_acres_per_citizen, avg_income_per_farmer = fetch_agricultural_data()
 
     return jsonify({'crop_type':crop_type,
                     'avg_area_acres':avg_area_acres,
@@ -124,28 +228,95 @@ def refresh_agricultural_data():
                     'avg_income_per_farmer':avg_income_per_farmer})
 
 
+
 @government_bp.route('/refresh_vaccination')
 @login_required
 @government_monitor_required
 @verification_required
 def refresh_vaccination():
-    conn = get_db_connection()
-    db = conn.cursor()
+    vaccine_type, num_citizens, num_citizens_vaccinated, num_citizens_vaccinated_all, num_citizens_not_vaccinated, top_5_centers = fetch_vaccination()
 
-    
+    return jsonify({'vaccine_type':vaccine_type,
+                    'num_citizens':num_citizens,
+                    'num_citizens_vaccinated':num_citizens_vaccinated,
+                    'num_citizens_vaccinated_all':num_citizens_vaccinated_all,
+                    'num_citizens_not_vaccinated':num_citizens_not_vaccinated,
+                    'top_5_centers':top_5_centers})
 
-    pass
+
 
 @government_bp.route('/refresh_census_data')
 @login_required
 @government_monitor_required
 @verification_required
-def refresh_vaccination_data():
-    pass
+def refresh_census_data():
+    years, population_male, population_female, births_male, births_female, deaths_male, deaths_female, marriages = fetch_census_data()
+
+    return jsonify({'years':years,
+                    'population_male':population_male,
+                    'population_female':population_female,
+                    'births_male':births_male,
+                    'births_female':births_female,
+                    'deaths_male':deaths_male,
+                    'deaths_female':deaths_female,
+                    'marriages':marriages})
+
+
 
 @government_bp.route('/refresh_environmental_data')
 @login_required
 @government_monitor_required
 @verification_required
 def refresh_environmental_data():
-    pass
+    dates, air_quality_index, water_quality, sanitation, air_dates, air_quality_index_5 = fetch_environmental_data()
+
+    return jsonify({'dates':dates,
+                    'air_quality_index':air_quality_index,
+                    'water_quality':water_quality,
+                    'sanitation':sanitation,
+                    'air_dates':air_dates,
+                    'air_quality_index_5':air_quality_index_5})
+
+
+
+
+
+@government_bp.route('/dashboard')
+@login_required
+@government_monitor_required
+@verification_required
+def dashboard():
+    crop_type, avg_area_acres, total_area_acres, avg_area_acres_per_citizen, avg_income_per_farmer = fetch_agricultural_data()
+
+    vaccine_type, num_citizens, num_citizens_vaccinated, num_citizens_vaccinated_all, num_citizens_not_vaccinated, top_5_centers = fetch_vaccination()
+
+    years, population_male, population_female, births_male, births_female, deaths_male, deaths_female, marriages = fetch_census_data()
+
+    dates, air_quality_index, water_quality, sanitation, air_dates, air_quality_index_5 = fetch_environmental_data()
+
+    return render_template('Government/dashboard.html', 
+                           crop_type=crop_type, 
+                           avg_area_acres=avg_area_acres, 
+                           total_area_acres=total_area_acres, 
+                           avg_area_acres_per_citizen=avg_area_acres_per_citizen,
+                           avg_income_per_farmer=avg_income_per_farmer,
+                           vaccine_type=vaccine_type,
+                           num_citizens=num_citizens,
+                           num_citizens_vaccinated=num_citizens_vaccinated,
+                           num_citizens_vaccinated_all=num_citizens_vaccinated_all,
+                           num_citizens_not_vaccinated=num_citizens_not_vaccinated,
+                           top_5_centers=top_5_centers,
+                           years=years,
+                           population_male=population_male,
+                           population_female=population_female,
+                           births_male=births_male,
+                           births_female=births_female,
+                           deaths_male=deaths_male,
+                           deaths_female=deaths_female,
+                           marriages=marriages,
+                           date=dates,
+                           air_quality_index=air_quality_index,
+                           water_quality=water_quality,
+                           sanitation=sanitation,
+                           air_date=air_dates,
+                           air_quality_index_5=air_quality_index_5)
--- Project/Government/static/js/refresh_tabs.js
+++ Project/Government/static/js/refresh_tabs.js
@@ -1,7 +1,7 @@
 const agri_tab = document.getElementById('agricultural-data-tab');
-const vac_tab = document.getElementById('vaccination-data-tab');
+const vac_tab = document.getElementById('vaccination-tab');
 const census_tab = document.getElementById('census-data-tab');
-const environment_tab = document.getElementById('environment-data-tab');
+const environment_tab = document.getElementById('environmental-data-tab');
 
 function toFixed(value, precision) {
     var power = Math.pow(10, precision || 0);
@@ -31,7 +31,7 @@ agri_tab.addEventListener('click', () => {
             // remove extra table rows if needed
             if (table_rows.length > crop_type.length)
             {
-                for (let i = crop_type.length + 1; i < table_rows.length; i++)
+                for (let i = table_rows.length - 1; i > crop_type.length; i++)
                 {
                     table.deleteRow(i);
                 }
@@ -60,6 +60,237 @@ agri_tab.addEventListener('click', () => {
             p2.innerHTML = 'Average Income of a farmer in the village is close to ' + toFixed(avg_income_per_farmer,2);
 
             console.log('Agricultural data refreshed');
+        }
+    });
+});
+
+vac_tab.addEventListener('click', () => {
+    $.ajax({
+        url: '/government/refresh_vaccination',
+        type: 'GET',
+        success: function (response) 
+        {
+            let vaccine_type = response['vaccine_type'];
+            let num_citizens = response['num_citizens'];
+            let num_citizens_vaccinated = response['num_citizens_vaccinated'];
+            let num_citizens_vaccinated_all = response['num_citizens_vaccinated_all'];
+            let num_citizens_not_vaccinated = response['num_citizens_not_vaccinated'];
+            let top_5_centers = response['top_5_centers']; 
+
+            // update table rows with new data
+            let table_vac = document.getElementById('vaccination-table');
+            let table_rows = table_vac.getElementsByTagName('tr');
+            for (let i = 1; i < Math.min(vaccine_type.length+1, table_rows.length); i++)
+            {
+                table_rows[i].getElementsByTagName('td')[0].innerHTML = vaccine_type[i-1];
+                table_rows[i].getElementsByTagName('td')[1].innerHTML = num_citizens[i-1];
+            }
+
+            // remove extra table rows if needed
+            if (table_rows.length > vaccine_type.length)
+            {
+                for (let i = table_rows.length - 1; i > vaccine_type.length; i++)
+                {
+                    table.deleteRow(i);
+                }
+            }
+            else {
+                // add additonal table row tr elements if needed, i.e. if vaccine_name.length > table_rows.length
+                for (let i = table_rows.length; i <= vaccine_name.length; i++)
+                {
+                    let new_row = table_vac.insertRow(-1);
+                    let cell1 = new_row.insertCell(0);
+                    let cell2 = new_row.insertCell(1);
+                    cell1.innerHTML = vaccine_type[i-1];
+                    cell2.innerHTML = num_citizens[i-1];
+                }
+            }
+
+            // update top 5 vaccination centers
+            let table_top5 = document.getElementById('top5-centers-table');
+            let table_rows_top5 = table_top5.getElementsByTagName('tr');
+            for (let i = 1; i < Math.min(top_5_centers.length+1, table_rows_top5.length); i++)
+            {
+                table_rows_top5[i].getElementsByTagName('td')[0].innerHTML = top_5_centers[i-1];
+            }
+
+            // remove extra table rows if needed
+            if (table_rows_top5.length > top_5_centers.length)
+            {
+                for (let i = table_rows_top5.length - 1; i > top_5_centers.length; i++)
+                {
+                    table_top5.deleteRow(i);
+                }
+            }
+            else {
+                // add additonal table row tr elements if needed, i.e. if top_5_centers.length > table_rows_top5.length
+                for (let i = table_rows_top5.length; i <= top_5_centers.length; i++)
+                {
+                    let new_row = table_top5.insertRow(-1);
+                    let cell1 = new_row.insertCell(0);
+                    cell1.innerHTML = top_5_centers[i-1];
+                }
+            }
+
+            let p1 = document.getElementById('atleast-one-para');
+            let p2 = document.getElementById('all-vaccines-para');
+            let p3 = document.getElementById('not-vaccinated-para');
+
+            p1.innerHTML = 'Total Number of Citizens who have been administered atleast one vaccine is close to ' + num_citizens_vaccinated;
+            p2.innerHTML = 'Total Number of Citizens who have been administered all the available vaccine types is close to ' + num_citizens_vaccinated_all;
+            p3.innerHTML = 'Total Number of Citizens who have not been administered any vaccine is close to ' + num_citizens_not_vaccinated;
+
+            console.log('Vaccination data refreshed');
+        }
+    });
+});
+
+census_tab.addEventListener('click', () => {
+    $.ajax({
+        url: '/government/refresh_census_data',
+        type: 'GET',
+        success: function (response) 
+        {
+            let years = response['years'];
+            let population_male = response['population_male'];
+            let population_female = response['population_female'];
+            let births_male = response['births_male'];
+            let births_female = response['births_female'];
+            let deaths_male = response['deaths_male'];
+            let deaths_female = response['deaths_female'];
+            let marriages = response['marriages'];
+
+            // update table rows with new data
+            let table = document.getElementById('census-data-table');
+            let table_rows = table.getElementsByTagName('tr');
+            for (let i = 1; i < Math.min(years.length+1, table_rows.length); i++)
+            {
+                table_rows[i].getElementsByTagName('td')[0].innerHTML = years[i-1];
+                table_rows[i].getElementsByTagName('td')[1].innerHTML = population_male[i-1];
+                table_rows[i].getElementsByTagName('td')[2].innerHTML = population_female[i-1];
+                table_rows[i].getElementsByTagName('td')[3].innerHTML = births_male[i-1];
+                table_rows[i].getElementsByTagName('td')[4].innerHTML = births_female[i-1];
+                table_rows[i].getElementsByTagName('td')[5].innerHTML = deaths_male[i-1];
+                table_rows[i].getElementsByTagName('td')[6].innerHTML = deaths_female[i-1];
+                table_rows[i].getElementsByTagName('td')[7].innerHTML = marriages[i-1];
+            }
+
+            // remove extra table rows if needed
+            if (table_rows.length > years.length)
+            {
+                for (let i = table_rows.length - 1; i > years.length; i++)
+                {
+                    table.deleteRow(i);
+                }
+            }
+            else {
+                // add additonal table row tr elements if needed, i.e. if years.length > table_rows.length
+                for (let i = table_rows.length; i <= years.length; i++)
+                {
+                    let new_row = table.insertRow(-1);
+                    let cell1 = new_row.insertCell(0);
+                    let cell2 = new_row.insertCell(1);
+                    let cell3 = new_row.insertCell(2);
+                    let cell4 = new_row.insertCell(3);
+                    let cell5 = new_row.insertCell(4);
+                    let cell6 = new_row.insertCell(5);
+                    let cell7 = new_row.insertCell(6);
+                    let cell8 = new_row.insertCell(7);
+                    cell1.innerHTML = years[i-1];
+                    cell2.innerHTML = population_male[i-1];
+                    cell3.innerHTML = population_female[i-1];
+                    cell4.innerHTML = births_male[i-1];
+                    cell5.innerHTML = births_female[i-1];
+                    cell6.innerHTML = deaths_male[i-1];
+                    cell7.innerHTML = deaths_female[i-1];
+                    cell8.innerHTML = marriages[i-1];
+                }
+            }
+
+            console.log('Census data refreshed');
+        }
+    });
+});
+
+environment_tab.addEventListener('click', () => {
+    $.ajax({
+        url: '/government/refresh_environmental_data',
+        type: 'GET',
+        success: function (response) 
+        {
+            let dates = response['dates'];
+            let air_quality_index = response['air_quality_index'];
+            let water_quality_index = response['water_quality_index'];
+            let sanitation = response['sanitation'];
+            let air_dates = response['air_dates'];
+            let air_quality_index_5 = response['air_quality_index_5'];
+
+            // update table rows with new data
+            let table = document.getElementById('last5-environmental-table');
+            let table_rows = table.getElementsByTagName('tr');
+            for (let i = 1; i < Math.min(dates.length+1, table_rows.length); i++)
+            {
+                table_rows[i].getElementsByTagName('td')[0].innerHTML = dates[i-1];
+                table_rows[i].getElementsByTagName('td')[1].innerHTML = air_quality_index[i-1];
+                table_rows[i].getElementsByTagName('td')[2].innerHTML = water_quality_index[i-1];
+                table_rows[i].getElementsByTagName('td')[3].innerHTML = sanitation[i-1];
+            }
+
+            // remove extra table rows if needed
+            if (table_rows.length > dates.length)
+            {
+                for (let i = table_rows.length - 1; i > dates.length; i++)
+                {
+                    table.deleteRow(i);
+                }
+            }
+            else {
+                // add additonal table row tr elements if needed, i.e. if dates.length > table_rows.length
+                for (let i = table_rows.length; i <= dates.length; i++)
+                {
+                    let new_row = table.insertRow(-1);
+                    let cell1 = new_row.insertCell(0);
+                    let cell2 = new_row.insertCell(1);
+                    let cell3 = new_row.insertCell(2);
+                    let cell4 = new_row.insertCell(3);
+                    cell1.innerHTML = dates[i-1];
+                    cell2.innerHTML = air_quality_index[i-1];
+                    cell3.innerHTML = water_quality_index[i-1];
+                    cell4.innerHTML = sanitation[i-1];
+                }
+            }
+
+
+            // update air quality index for the worst 5 days
+            let table_air = document.getElementById('worst5-air-table');
+            let table_rows_air = table_air.getElementsByTagName('tr');
+            for (let i = 1; i < Math.min(air_dates.length+1, table_rows_air.length); i++)
+            {
+                table_rows_air[i].getElementsByTagName('td')[0].innerHTML = air_dates[i-1];
+                table_rows_air[i].getElementsByTagName('td')[1].innerHTML = air_quality_index_5[i-1];
+            }
+
+            // remove extra table rows if needed
+            if (table_rows_air.length > air_dates.length)
+            {
+                for (let i = table_rows_air.length - 1; i > air_dates.length; i++)
+                {
+                    table_air.deleteRow(i);
+                }
+            }
+            else {
+                // add additonal table row tr elements if needed, i.e. if air_dates.length > table_rows_air.length
+                for (let i = table_rows_air.length; i <= air_dates.length; i++)
+                {
+                    let new_row = table_air.insertRow(-1);
+                    let cell1 = new_row.insertCell(0);
+                    let cell2 = new_row.insertCell(1);
+                    cell1.innerHTML = air_dates[i-1];
+                    cell2.innerHTML = air_quality_index_5[i-1];
+                }
+            }
+
+            console.log('Environmental data refreshed');
         }
     });
 });
